62a6baa304fd743d61c062a8b5862e8f349eebc1
citadorapa <110743149+citadorapa@users.noreply.github.com>
2022-08-06 15:12:00 -0500

    Initial commit

---
+++ citador.js
@@ -0,0 +1,59 @@
+function citaropcion1() {
+
+	var titulo1 = document.getElementById('titulo_web').value;
+	document.getElementById("res_titulo_web").innerHTML = titulo1;
+
+	var autor1 = document.getElementById('autor_web').value;
+	document.getElementById("res_autor_web").innerHTML = autor1;
+
+	fecha1 = document.getElementById('fecha_web').value;
+	document.getElementById("res_fecha_web").innerHTML = fecha1;
+
+	fechanew1 = document.getElementById('fechanew_web').value;
+	document.getElementById("res_fechanew_web").innerHTML = fechanew1;
+
+	fechanew1 = document.getElementById('autorempins_web').value;
+	document.getElementById("res_autorempins_web").innerHTML = fechanew1;
+
+	url1 = document.getElementById('url_web').value;
+	document.getElementById("res_url_web").innerHTML = url1;
+
+}
+
+function citaropcion2() {
+
+	var titulo2 = document.getElementById('titulo_libro').value;
+	document.getElementById("res_titulo_libro").innerHTML = titulo2;
+
+	var autor2 = document.getElementById('autor_libro').value;
+	document.getElementById("res_autor_libro").innerHTML = autor2;
+
+	fecha2 = document.getElementById('fecha_libro').value;
+	document.getElementById("res_fecha_libro").innerHTML = fecha2;
+
+	lugar1 = document.getElementById('lugar_libro').value;
+	document.getElementById("res_lugar_libro").innerHTML = lugar1;
+
+	editorial1 = document.getElementById('editorial_libro').value;
+	document.getElementById("res_editorial_libro").innerHTML = editorial1;
+
+}
+
+function citaropcion3() {
+
+	var titulo3 = document.getElementById('titulo_periodico').value;
+	document.getElementById("res_titulo_periodico").innerHTML = titulo3;
+
+	var autor3 = document.getElementById('autor_periodico').value;
+	document.getElementById("res_autor_periodico").innerHTML = autor3;
+
+	fecha3 = document.getElementById('fecha_periodico').value;
+	document.getElementById("res_fecha_periodico").innerHTML = fecha3;
+
+	nombre1 = document.getElementById('nombre_periodico').value;
+	document.getElementById("res_nombre_periodico").innerHTML = nombre1;
+
+	periodico1 = document.getElementById('paginas_periodico').value;
+	document.getElementById("res_paginas_periodico").innerHTML = periodico1;
+
+}
