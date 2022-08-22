0009e7ff715baa172d1722c9ea27f2d6639bec84
citadorapa <110743149+citadorapa@users.noreply.github.com>
2022-08-21 22:09:27 -0500

    Update citador.js

--- citador.js
+++ citador.js
@@ -57,3 +57,63 @@ function citaropcion3() {
 	document.getElementById("res_paginas_periodico").innerHTML = periodico1;
 
 }
+
+function citaropcion4() {
+
+	var titulo4 = document.getElementById('titulo_pdf').value;
+	document.getElementById("res_titulo_pdf").innerHTML = titulo4;
+
+	var autor4 = document.getElementById('autor_pdf').value;
+	document.getElementById("res_autor_pdf").innerHTML = autor4;
+
+	fecha4 = document.getElementById('fecha_pdf').value;
+	document.getElementById("res_fecha_pdf").innerHTML = fecha4;
+
+	url2 = document.getElementById('url_pdf').value;
+	document.getElementById("res_url_pdf").innerHTML = url2;
+
+}
+
+function citaropcion5() {
+
+	var titulo5 = document.getElementById('titulo_cancion').value;
+	document.getElementById("res_titulo_cancion").innerHTML = titulo5;
+
+	var autor5 = document.getElementById('autor_cancion').value;
+	document.getElementById("res_autor_cancion").innerHTML = autor5;
+
+	fecha5 = document.getElementById('fecha_cancion').value;
+	document.getElementById("res_fecha_cancion").innerHTML = fecha5;
+
+	var album1 = document.getElementById('album_cancion').value;
+	document.getElementById("res_album_cancion").innerHTML = album1;
+
+	var grabadora1 = document.getElementById('grabadora_cancion').value;
+	document.getElementById("res_grabadora_cancion").innerHTML = grabadora1;
+
+	url3 = document.getElementById('url_cancion').value;
+	document.getElementById("res_url_cancion").innerHTML = url3;
+
+}
+
+function citaropcion6() {
+
+	var titulo6 = document.getElementById('titulo_video').value;
+	document.getElementById("res_titulo_video").innerHTML = titulo6;
+
+	var autor5 = document.getElementById('autor_video').value;
+	document.getElementById("res_autor_video").innerHTML = autor5;
+
+	fecha5 = document.getElementById('fecha_video').value;
+	document.getElementById("res_fecha_video").innerHTML = fecha5;
+
+	var usuario1 = document.getElementById('usuario_video').value;
+	document.getElementById("res_usuario_video").innerHTML = usuario1;
+
+	var plataforma1 = document.getElementById('plataforma_video').value;
+	document.getElementById("res_plataforma_video").innerHTML = plataforma1;
+
+	url4 = document.getElementById('url_video').value;
+	document.getElementById("res_url_video").innerHTML = url4;
+
+}
